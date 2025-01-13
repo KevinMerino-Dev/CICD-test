295890ec9e4560993a1d39c3f7d4d77e127330a0
KevinMerino-Dev <k.merino.ing@gmail.com>
2025-01-13 16:32:04 -0300

    Mejoras en Test y separación de Build and Test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-*Este es un Codigo de Pruebas
+<header>
+
+<!-- Este es un codigo de pruebas realizados para CityLab BIO-BIO
+-->
+
+# Sistema CI/CD
+
+_Este es un Codigo de Pruebas_
+
+</header>
+
+## Explicación
+
+**¿Qué hace main.py?**: Este codigo calcula los días faltantes para que se acabe el año gracias a la biblioteca "datatime".
--- test_main.py
+++ test_main.py
@@ -2,8 +2,8 @@ import unittest
 from datetime import datetime
 from main import dias_restantes
 
-
 class TestMain(unittest.TestCase):
+    
     def test_calculate_days_left(self):
         # Calcular el número de días restantes hasta el fin de año
         today = datetime.now()
@@ -13,5 +13,10 @@ class TestMain(unittest.TestCase):
         # Comparar el valor calculado por la función con el valor esperado
         self.assertEqual(dias_restantes(), expected_result)
 
+
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        unittest.main(exit=False)
+    except SystemExit:
+        pass
+    print(f"No hay errores y: faltan {dias_restantes()} días")
